7f2e4052f5920c4fa03e962a97fadddceed82dcb
Cookie Celeste <fbiciohenry07@gmail.com>
2025-08-07 09:25:10 -0300

    att

--- src/inicio.jsx
+++ src/inicio.jsx
@@ -16,14 +16,17 @@ export default function Inicio(){
     
     return(
         <div className="container-pagina">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Bakbak+One&family=Comic+Relief:wght@400;700&display=swap');
+            </style>
             <div className="cabecalho">
-                <div className="texto-cabecalho">
+                <div className="texto-cabecalho bakbak-one-regular">
             <img className="logo" src={logo} height="50px"/>
-            <h3 >Intituto Sócial Nossa Senhora de Fatima</h3>
+            <h3 >Instituto Social Nossa Senhora de Fátima</h3>
             </div>
             <div className="email_lock">
             <a href="">secretaria@acaonsfatima.org.br</a>
-            <p>Av.cel.octaviano de freitas costa, 363</p>
+            <a href="https://www.google.com.br/maps/place/Instituto+Social+Nossa+Senhora+de+F%C3%A1tima/@-23.6803333,-46.7079309,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce502d2289a843:0x14406b17b30d0174!8m2!3d-23.6803333!4d-46.7079309!16s%2Fg%2F1ptxj22k2?entry=ttu&g_ep=EgoyMDI1MDgwNC4wIKXMDSoASAFQAw%3D%3D">Av. Coronel Octaviano de Freitas Costa, 463 - Socorro, São Paulo - SP, 04773-000</a>
             
             </div>
 
@@ -44,7 +47,7 @@ export default function Inicio(){
             <div className="carta_curso">
                 <div className="apresenta">
                     <img src={ingles} className="img_curso"/>
-                    <h3>ingles intermediario e avançado</h3>
+                    <h3>Inglês intermediário e avançado</h3>
                 </div>
                 <div className="descri">
                 <p>balalallallalallalalallalalla<br/>lalalallalalalllalalalaallalall</p>
@@ -54,7 +57,7 @@ export default function Inicio(){
             <div className="carta_curso">
                 <div className="apresenta">
                     <img src={info} className="img_curso"/>
-                    <h3>ingles intermediario e avançado</h3>
+                    <h3>Informática básica e avançada</h3>
                 </div>
                 <div className="descri">
                 <p>balalallallalallalalallalalla<br/>lalalallalalalllalalalaallalall</p>
@@ -64,7 +67,7 @@ export default function Inicio(){
             <div className="carta_curso">
                 <div className="apresenta">
                     <img src={olho} className="img_curso"/>
-                    <h3>ingles intermediario e avançado</h3>
+                    <h3>Comunicação visual</h3>
                 </div>
                 <div className="descri">
                 <p>balalallallalallalalallalalla<br/>lalalallalalalllalalalaallalall</p>
@@ -74,7 +77,7 @@ export default function Inicio(){
             <div className="carta_curso">
                 <div className="apresenta">
                     <img src={anpul} className="img_curso"/>
-                    <h3>ingles intermediario e avançado</h3>
+                    <h3>Administração</h3>
                 </div>
                 <div className="descri">
                 <p>balalallallalallalalallalalla<br/>lalalallalalalllalalalaallalall</p>
@@ -84,7 +87,7 @@ export default function Inicio(){
             <div className="carta_curso">
                 <div className="apresenta">
                     <img src={carro} className="img_curso"/>
-                    <h3>ingles intermediario e avançado</h3>
+                    <h3>Eletromecânica de Autos</h3>
                 </div>
                 <div className="descri">
                 <p>balalallallalallalalallalalla<br/>lalalallalalalllalalalaallalall</p>
@@ -94,7 +97,7 @@ export default function Inicio(){
             <div className="carta_curso">
                 <div className="apresenta">
                     <img src={eletro} className="img_curso"/>
-                    <h3>ingles intermediario e avançado</h3>
+                    <h3>Eletricista Instalador</h3>
                 </div>
                 <div className="descri">
                 <p>balalallallalallalalallalalla<br/>lalalallalalalllalalalaallalall</p>
